2f6bcb3c61045aa73b82a218b970924642f4141e
SurapureddySusmitha21 <surapureddysusmitha@gmail.com>
2023-10-08 09:24:30 +0000

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,34 @@
+// Write your code here
+import {Component} from 'react'
+
+import Message from '../Message'
+import Login from '../Login'
+import Logout from '../Logout'
+
+import './index.css'
+
+class Home extends Component {
+  state = {isLoggedIn: false}
+
+  onClickButton = () => {
+    this.setState(prevsState => ({isLoggedIn: !prevsState.isLoggedIn}))
+  }
+
+  render() {
+    const {isLoggedIn} = this.state
+    return (
+      <div className="app-container">
+        <div className="bg-container">
+          <Message isLoggedIn={isLoggedIn} />
+          {isLoggedIn ? (
+            <Logout logout={this.onClickButton} />
+          ) : (
+            <Login login={this.onClickButton} />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
---
+++ src/components/Message/index.js
@@ -0,0 +1,10 @@
+// Write your code here
+import './index.css'
+
+const Message = props => {
+  const {isLoggedin} = props
+  const message = isLoggedin ? 'Welcome User' : 'Please Login'
+
+  return <h1 className="message">{message}</h1>
+}
+export default Message
